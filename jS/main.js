da60b1de602d8d4e3d3278645e95b0cb260bebfa
Aum-S2 <sorasitdb7375@hotmail.com>
2019-07-17 15:00:36 +0700

    smooth scrooling n fixed text

--- jS/main.js
+++ jS/main.js
@@ -11,6 +11,16 @@ function initMap() {
   const marker = new google.maps.Marker ({position: loc, map: map});
 }
 
+
+// Sticky Menu Background
+window.addEventListener ('scroll', function() {
+  if (this.window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
   if(this.hash !=='') {
@@ -20,9 +30,9 @@ $('#navbar a, .btn').on('click', function(event) {
 
     $('html, body').animate(
       {
-        scrollTop: $(hash).offset().top - 100
+        scrollTop: $(hash).offset().top - 78
       },
-      800
+      420
     );
   }
 });
